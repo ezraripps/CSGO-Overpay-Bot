6a5efddf1d842e95f1c463e459758d858030f7e8
Arze1 <Arze1@users.noreply.github.com>
2016-12-13 18:25:57 -0500

    REMOVED ALL SPACES REPLACES WITH TABS

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ var SteamCommunity = require('steamcommunity');
 var community = new SteamCommunity();
 var fs = require('fs');
 var manager = new TradeOfferManager({
-    "steam": client,
-    "domain": "example.com",
-    "language": "en"
+	"steam": client,
+	"domain": "example.com",
+	"language": "en"
 });
 var request = require('request'),
-    url = 'https://api.csgofast.com/price/all';
+	url = 'https://api.csgofast.com/price/all';
 
 function getPrice() {
 	request(url, (error, response, body) => {
@@ -48,11 +48,11 @@ client.on('webSession', function(sessionID, cookies) {
 });
 
 manager.on('pollData', function(pollData) {
-    fs.writeFile('polldata.json', JSON.stringify(pollData));
+	fs.writeFile('polldata.json', JSON.stringify(pollData));
 });
 
 if (fs.existsSync('polldata.json')) {
-    manager.pollData = JSON.parse(fs.readFileSync('polldata.json'));
+	manager.pollData = JSON.parse(fs.readFileSync('polldata.json'));
 }
 
 client.on('loggedOn', function(details) {
@@ -73,7 +73,6 @@ client.on('webSession', function(sessionID, cookies) {
 			process.exit(1);
 			return;
 		}
-
 		console.log("Got API key: " + manager.apiKey);
 	});
 
@@ -82,12 +81,12 @@ client.on('webSession', function(sessionID, cookies) {
 });
 
 function exists(id) {
-  for (var i = 0; i < config.acceptSteamIDS.length; i++) {
-    if (config.acceptSteamIDS[i] == id) {
-      return true;
-    }
-    return false;
-  }
+	for (var i = 0; i < config.acceptSteamIDS.length; i++) {
+		if (config.acceptSteamIDS[i] == id) {
+			return true;
+		}
+	return false;
+	}
 }
 function getPrices(offer) {
 	var prices = require('./prices.json');
@@ -95,7 +94,6 @@ function getPrices(offer) {
 	if (offer == null) {
 		offer = {'market_hash_name': 200};
 	}
-
 	for (var i = 0; i < offer.length; i++) {
 		x = x +prices[offer[i].market_hash_name];
 	}
@@ -104,29 +102,29 @@ function getPrices(offer) {
 
 manager.on('newOffer', function(offer) {
 	var ID = offer.partner.getSteamID64();
-	console.log("New offer #" + offer.id + " from " + ID);
-  function accept() {
-    offer.accept(function(err) {
-      if (err) {
-          console.log("Unable to accept offer: " + err.message);
-      } else {
-          community.checkConfirmations();
-      }
-    });
-  }
+		console.log("New offer #" + offer.id + " from " + ID);
+	function accept() {
+		offer.accept(function(err) {
+			if (err) {
+				console.log("Unable to accept offer: " + err.message);
+			} else {
+				community.checkConfirmations();
+			}
+		});
+	}
 	if (offer.itemsToGive.length == 0) {
 		client.chatMessage(ID, config.options.chatResponse.donation);
 		console.log(ID + ' Just Donated us skins!');
-    accept();
+		accept();
 	} else {
-	  if (getPrices(offer.itemsToGive) > getPrices(offer.itemsToReceive)*config.options.percentamount) {
+		if (getPrices(offer.itemsToGive) > getPrices(offer.itemsToReceive)*config.options.percentamount) {
 			client.chatMessage(ID, config.options.chatResponse.tradeDeclined);
 				offer.decline(function(err) {
-		      if (err) {
-		          console.log("Unable to decline offer: " + err.message);
-		      }
-		    });
-	  } else if (getPrices(offer.itemsToGive) < getPrices(offer.itemsToReceive)*config.options.percentamount) {
+			if (err) {
+				console.log("Unable to decline offer: " + err.message);
+			}
+		});
+		} else if (getPrices(offer.itemsToGive) < getPrices(offer.itemsToReceive)*config.options.percentamount) {
 			client.chatMessage(ID, config.options.chatResponse.tradeAccepted);
 			accept();
 		}
