654658771e45fb3e08acd842fb7e8ed63afa6eb1
Arze1 <ezra1135@gmail.com>
2016-12-13 17:56:14 -0500

    Added All files

---
+++ bot.js
@@ -0,0 +1,135 @@
+var SteamUser = require('steam-user');
+var client = new SteamUser();
+var TradeOfferManager = require('steam-tradeoffer-manager');
+var config = require('./config.json');
+var SteamTotp = require('steam-totp');
+var SteamCommunity = require('steamcommunity');
+var community = new SteamCommunity();
+var fs = require('fs');
+var manager = new TradeOfferManager({
+    "steam": client,
+    "domain": "example.com",
+    "language": "en"
+});
+var request = require('request'),
+    url = 'https://api.csgofast.com/price/all';
+
+		function getPrice() {
+		    request(url, (error, response, body) => {
+		        if (!error && response.statusCode === 200) {
+
+		            var jsonResponse = JSON.parse(body);
+								fs.writeFile('prices.json', body);
+		        } else {
+		            console.log("Got an error: ", error, ", status code: ", response.statusCode);
+		        }
+		    });
+		}
+		getPrice();
+setInterval(function() {
+	getPrice();
+}, 30 * 1000);
+function logOn() {
+	client.logOn({
+		"accountName": config.username,
+		"password": config.password,
+		"twoFactorCode": SteamTotp.generateAuthCode(config.sharedSecret)
+	});
+}
+logOn();
+
+client.on('webSession', function(sessionID, cookies) {
+	manager.setCookies(cookies, function(err) {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+			return;
+		}
+	});
+});
+
+manager.on('pollData', function(pollData) {
+    fs.writeFile('polldata.json', JSON.stringify(pollData));
+});
+
+if (fs.existsSync('polldata.json')) {
+    manager.pollData = JSON.parse(fs.readFileSync('polldata.json'));
+}
+
+client.on('loggedOn', function(details) {
+	console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+	client.setPersona(SteamUser.EPersonaState.Online);
+});
+
+var manager = new TradeOfferManager({
+	"steam": client,
+	"domain": "nothinghere",
+	"language": "en"
+});
+
+client.on('webSession', function(sessionID, cookies) {
+	manager.setCookies(cookies, function(err) {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+			return;
+		}
+
+		console.log("Got API key: " + manager.apiKey);
+	});
+
+	community.setCookies(cookies);
+	community.startConfirmationChecker(30000, config.identitySecret);
+});
+
+function exists(id) {
+  for (var i = 0; i < config.acceptSteamIDS.length; i++) {
+    if (config.acceptSteamIDS[i] == id) {
+      return true;
+    }
+    return false;
+  }
+}
+function getPrices(offer) {
+	var prices = require('./prices.json');
+	var x = 0;
+	if (offer == null) {
+		offer = {'market_hash_name': 200};
+	}
+
+	for (var i = 0; i < offer.length; i++) {
+		x = x +prices[offer[i].market_hash_name];
+	}
+	return x;
+}
+
+manager.on('newOffer', function(offer) {
+	var ID = offer.partner.getSteamID64();
+	console.log("New offer #" + offer.id + " from " + ID);
+  function accept() {
+    offer.accept(function(err) {
+      if (err) {
+          console.log("Unable to accept offer: " + err.message);
+      } else {
+          community.checkConfirmations();
+      }
+    });
+  }
+	if (offer.itemsToGive.length == 0) {
+		client.chatMessage(ID, config.options.chatResponse.donation);
+		console.log(ID + ' Just Donated us skins!');
+    accept();
+	} else {
+	  if (getPrices(offer.itemsToGive) > getPrices(offer.itemsToReceive)*config.options.percentamount) {
+			client.chatMessage(ID, config.options.chatResponse.tradeDeclined);
+				offer.decline(function(err) {
+		      if (err) {
+		          console.log("Unable to decline offer: " + err.message);
+		      }
+		    });
+	  } else if (getPrices(offer.itemsToGive) < getPrices(offer.itemsToReceive)*config.options.percentamount) {
+			client.chatMessage(ID, config.options.chatResponse.tradeAccepted);
+			accept();
+		}
+	}
+});
---
+++ node_modules/steam-tradeoffer-manager/README.md
@@ -0,0 +1,26 @@
+# Steam Trade Offer Manager for Node.js
+[![npm version](https://img.shields.io/npm/v/steam-tradeoffer-manager.svg)](https://npmjs.com/package/steam-tradeoffer-manager)
+[![npm downloads](https://img.shields.io/npm/dm/steam-tradeoffer-manager.svg)](https://npmjs.com/package/steam-tradeoffer-manager)
+[![dependencies](https://img.shields.io/david/DoctorMcKay/node-steam-tradeoffer-manager.svg)](https://david-dm.org/DoctorMcKay/node-steam-tradeoffer-manager)
+[![license](https://img.shields.io/npm/l/steam-tradeoffer-manager.svg)](https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/blob/master/LICENSE)
+[![paypal](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=N36YVAT42CZ4G&item_name=node%2dsteam%2dtradeoffer%2dmanager&currency_code=USD)
+
+This module is designed to be a completely self-contained manager for
+[Steam trade offers](https://steamcommunity.com/my/tradeoffers).
+
+Instead of being just a simple API wrapper, it's designed to take most of the work out of implementing trade offers in
+your application.
+
+**You absolutely need Node.js v4.0.0 or later or this won't work.**
+
+Install it from [npm](https://www.npmjs.com/package/steam-tradeoffer-manager) or check out the
+[wiki](https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/wiki) for documentation.
+
+**Have a question about the module or coding in general? *Do not create a GitHub issue.* GitHub issues are for feature
+requests and bug reports. Instead, post in the [dedicated forum](https://dev.doctormckay.com/forum/9-node-steam-tradeoffer-manager/).
+Such issues may be ignored!**
+
+# Support
+
+Report bugs on the [issue tracker](https://github.com/DoctorMcKay/node-steam-tradeoffer-manager/issues) or ask questions
+on the [dedicated forum](https://dev.doctormckay.com/forum/9-node-steam-tradeoffer-manager/).
