1c395f78cee901e7d285a678e87fcf62567d1bc6
Arze1 <ezra1135@gmail.com>
2016-12-16 16:57:41 -0500

    Code cleanup with comments

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+/*
+	Initalizing ALL Modules
+*/
 const SteamUser = require('steam-user');
 const TradeOfferManager = require('steam-tradeoffer-manager');
 const SteamTotp = require('steam-totp');
@@ -14,32 +17,30 @@ const manager = new TradeOfferManager({
 	language: 'en'
 });
 
-const priceUrl = 'https://api.csgofast.com/price/all';
+//Priceing API url
+const priceUrl = 'https://api.csgofast.com/price/all'; //Not updated anymore Will update url once i find a better free one
 
 function getPrices(offer) {
 	let offervalue = 0;
 
 	if (offer) {
-		let prices = require('./prices.json');
 
+		let prices = require('./prices.json'); //Requiring price file
+
+		//Loop through offer and get total price
 		offer.forEach((item) => {
 			offervalue += prices[item.market_hash_name];
 		});
 	}
 
-	return offervalue;
+	return offervalue; //Return Total offer value
 }
 
-function acceptOffer(offer) {
-	offer.accept((err) => {
-		if (err) {
-			console.log(`Unable to accept offer: ${err.message}`);
-		} else {
-			community.checkConfirmations();
-		}
-	});
-}
 
+
+/*
+	Getting price from API
+*/
 function getPrice() {
 	request(priceUrl, (error, response, body) => {
 		if (!error && response.statusCode === 200) {
@@ -50,23 +51,72 @@ function getPrice() {
 	});
 }
 
-getPrice();
-setInterval(getPrice, config.options.priceRefreshInterval * 1000);
+getPrice(); //Iniatate First price request
 
+setInterval(getPrice, config.options.priceRefreshInterval * 1000); //Auto Refresh price
+
+
+
+
+/*
+	OFFER HANDLING
+*/
+//function to accept a offer
+function acceptOffer(offer) {
+	offer.accept((err) => {
+		if (err) {
+			console.log(`Unable to accept offer: ${err.message}`);
+		} else {
+			community.checkConfirmations();
+		}
+	});
+}
+
+manager.on('newOffer', function(offer) {
+	const partnerid = offer.partner.getSteamID64(); //Getting Offer partner steamid
+
+	console.log(`New offer # ${offer.id} from ${partnerid}`);
+
+	if (!offer.itemsToGive.length) {
+		console.log(`${partnerid} just donated us skins!`);
+
+		client.chatMessage(partnerid, config.options.chatResponse.donation); //Sending message for donations
+		acceptOffer(offer);
+	} else {
+		if (getPrices(offer.itemsToGive) > getPrices(offer.itemsToReceive) * config.options.percentamount) {
+			client.chatMessage(partnerid, config.options.chatResponse.tradeDeclined); //Sending message when trade declined
+			offer.decline(function(err) { //declineing offer
+				if (err) {
+					console.log(`Unable to decline offer: ${err.message}`);
+				}
+			});
+		} else if (getPrices(offer.itemsToGive) <= getPrices(offer.itemsToReceive) * config.options.percentamount) {
+			client.chatMessage(partnerid, config.options.chatResponse.tradeAccepted); //Sending message for accepting offer
+			acceptOffer(offer); //accepting offer
+		}
+	}
+});
+
+/*
+	Polling Steam and Logging On
+*/
+
+//Logging In
 client.logOn({
 	accountName: config.username,
 	password: config.password,
 	twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
 });
 
+//Refresh polldata.json
 manager.on('pollData', function(pollData) {
 	fs.writeFile('polldata.json', JSON.stringify(pollData));
 });
-
 if (fs.existsSync('polldata.json')) {
 	manager.pollData = JSON.parse(fs.readFileSync('polldata.json'));
 }
 
+//Logged ON listener
 client.on('loggedOn', function(details) {
 	console.log(`Logged into Steam as ${client.steamID.getSteam3RenderedID()}`);
  	client.setPersona(SteamUser.Steam.EPersonaState.Online,config.botname);
@@ -82,30 +132,5 @@ client.on('webSession', function(sessionID, cookies) {
 	});
 
 	community.setCookies(cookies);
-	community.startConfirmationChecker(config.options.confirmationInterval, config.identitySecret);
-});
-
-manager.on('newOffer', function(offer) {
-	const partnerid = offer.partner.getSteamID64();
-
-	console.log(`New offer # ${offer.id} from ${partnerid}`);
-
-	if (!offer.itemsToGive.length) {
-		console.log(`${partnerid} just donated us skins!`);
-
-		client.chatMessage(partnerid, config.options.chatResponse.donation);
-		acceptOffer(offer);
-	} else {
-		if (getPrices(offer.itemsToGive) > getPrices(offer.itemsToReceive) * config.options.percentamount) {
-			client.chatMessage(partnerid, config.options.chatResponse.tradeDeclined);
-			offer.decline(function(err) {
-				if (err) {
-					console.log(`Unable to decline offer: ${err.message}`);
-				}
-			});
-		} else if (getPrices(offer.itemsToGive) <= getPrices(offer.itemsToReceive) * config.options.percentamount) {
-			client.chatMessage(partnerid, config.options.chatResponse.tradeAccepted);
-			acceptOffer(offer);
-		}
-	}
+	community.startConfirmationChecker(config.options.confirmationInterval, config.identitySecret); //Starting checker for mobile confirmations
 });
