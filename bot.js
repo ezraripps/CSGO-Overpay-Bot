c4e506635930a8732e7d0cfcde0ac831a6c5d121
Arze1 <Arze1@users.noreply.github.com>
2017-03-14 11:11:18 -0400

    added fix for non-csgo items

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ function priceItemsInOffer(offer) {
 		const prices = require('./prices.json'); //Requiring price file
 		//Loop through offer and get total price
 		for (var x in offer) {
-			offerValue += prices[offer[x].market_hash_name]
+			prices[offer[x].market_hash_name] ? offerValue += prices[offer[x].market_hash_name] : null;
 		}
 	}
 	return offerValue;
